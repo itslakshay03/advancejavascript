71dc9fc8bc9f22c757255ad57819a2fdef64ed95
itslakshay03 <lakshayanand2004@gmail.com>
2025-04-23 11:31:21 +0530

    Map

---
+++ map.js
@@ -0,0 +1,35 @@
+// const people = [
+//     { name: "Alice", age: 25 },
+//     { name: "Bob", age: 30 },
+//     { name: "Charlie", age: 28 }
+// ];
+
+// const names = people.map(person => person.name);
+// console.log(names); 
+
+
+// const fruits=new Map();
+
+// fruits.set("apples",500);
+// fruits.set("bananas",300);
+// fruits.set("oranges",200);
+
+// console.log(fruits);
+
+// console.log(fruits.get("apples"));
+// console.log(typeof fruits);
+// fruits.delete("apple");
+// console.log(fruits);
+// // console.log(fruits.clear);
+// console.log(fruits.has())
+
+// let text = "";
+// const fruits = ["apple", "banana", "cherry"];
+
+// fruits.forEach(function(value, index) {
+//     text += index + ": " + value + " ";
+// });
+
+// console.log(text); 
+// Output: "0: apple 1: banana 2: cherry "
+
